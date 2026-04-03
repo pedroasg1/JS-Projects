a5a2cb2ec86e73108c05f46ca20a7e9f3f2f4798
Pedro Sánchez <158416336+pedroasg1@users.noreply.github.com>
2026-04-02 19:11:40 -0600

    Add files via upload

---
+++ convertCtoF.js
@@ -0,0 +1,7 @@
+const convertCtoF = arg => arg * (9/5) + 32
+
+console.log(convertCtoF(0)); //32°F 
+console.log(convertCtoF(-30)); //-22°F 
+console.log(convertCtoF(-10)); //14°F 
+console.log(convertCtoF(20)); //68°F 
+console.log(convertCtoF(30)); //86°F 
